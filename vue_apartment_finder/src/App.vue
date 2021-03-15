08af82098a9999d020a2d7a56d4428449ba8086f
Carlos Evelo <carlosevelo96@gmail.com>
2021-03-14 19:20:53 -0600

    app.vue nav and footer

--- vue_apartment_finder/src/App.vue
+++ vue_apartment_finder/src/App.vue
@@ -1,10 +1,43 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <div id="brand_name">
+        <router-link to="/">Apartment Finder</router-link>
+      </div>
+      <div id="main_links">
+        <div>
+        <router-link to="/">Home</router-link>
+      </div>
+      <div>
+        <router-link to="/buy">Buy</router-link>
+      </div>
+      <div>
+        <router-link to="/sell">Sell</router-link>
+      </div>
+      </div>
+      <div id="custom_links">
+        <div>
+        <router-link to="/signin">Sign In</router-link>
+      </div>
+      </div>
+      
     </div>
     <router-view />
+    <div id="footer">
+      <div class="footer_section">
+        <h2>Resources</h2>
+        <ul>
+          <li><a href="https://www.linkedin.com/in/carlos-evelo">LinkedIn</a></li>
+          <li><a href="https://github.com/BYU-CS-260-Winter-2021/lab-1-photography-website-carlosevelo">GitHub</a></li>
+        </ul>
+      </div>
+      <div class="footer_section">
+        <h2>Contact</h2>
+        <ul>
+          <li><a>Email</a></li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,18 +48,90 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
+  background-image: url(IMG_0310.jpeg);
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-clip: content-box;
 }
 
 #nav {
-  padding: 30px;
+  /* padding: 30px; */
+  background-image: linear-gradient(rgb(0,0,0,1),rgb(0,0,0,.0));
+  height: 200px;
+  padding-bottom: 120px;
+  display: grid;
+  grid-template-columns: repeat(9, 1fr);
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #d9e8f7;
+  text-decoration: none;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #e6dd60;
 }
+
+#brand_name {
+  grid-column: 1/4;
+  text-align: left;
+  padding: 20px;
+  font-size: 180%;
+  text-decoration: none;
+}
+
+#main_links {
+  grid-column: 4/7;
+  display: grid;
+  grid-template-columns: repeat(3,1fr);
+  padding: 20px;
+  font-size: 110%;
+}
+
+#custom_links {
+  grid-column: 8/10;
+  display: grid;
+  grid-template-columns: repeat(2,1fr);
+  padding: 20px;
+}
+
+#footer {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  padding: 10px;
+  position:relative;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-image: linear-gradient(rgb(0,0,0,0),rgb(0,0,0,1));
+  height: 200px;
+  padding-top: 60px;
+}
+    
+.footer_section {
+  display: grid;
+  grid-template-rows: 1fr 3fr;
+  justify-items: center;
+  width: 100%;
+  padding: 10px;
+  color: azure;
+}
+
+.footer_section ul {
+  list-style: none;
+  padding: 0px;
+}
+
+.footer_section li {
+  padding: 5px;
+}
+
+.footer_section a {
+  text-decoration: none;
+  color: #d9e8f7;
+}
+
 </style>
